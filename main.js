3699d1d986a21fbb371b89a55e2a50122f49a0e2
Carolina Valeria Mollo <carolinavmollo@gmail.com>
2023-09-16 19:59:53 -0300

    Filtro por género y paginación filtro

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 const container = document.getElementById("container");
 let currentPage = 1;
 let totalPages = 0;
+let currentFilter = "all"; // Variable para almacenar el filtro actual
 
-// Evento filtrar personajes
-const allBtn = document.getElementById("all");
-const femaleBtn = document.getElementById("Female");
-const maleBtn = document.getElementById("Male");
-const unknownBtn = document.getElementById("unknown");
+// Botones filtro
+const allBtn = document.querySelector(".all");
+const femaleBtn = document.querySelector(".female");
+const maleBtn = document.querySelector(".male");
+const unknownBtn = document.querySelector(".unknown");
 
-const getCharacters = (pageNumber) => {
+// Me trae los personajes
+const getCharacters = (pageNumber, filter) => {
   container.innerHTML = "";
-  fetch(`https://rickandmortyapi.com/api/character?page=${pageNumber}`)
+  const apiUrl =
+    filter === "all"
+      ? `https://rickandmortyapi.com/api/character?page=${pageNumber}`
+      : `https://rickandmortyapi.com/api/character/?gender=${filter}&page=${pageNumber}`;
+
+  fetch(apiUrl)
     .then((res) => res.json())
     .then((data) => {
       renderCharacters(data);
@@ -18,28 +25,27 @@ const getCharacters = (pageNumber) => {
     });
 };
 
-// getCharacters();
-
+// Función para renderizar personajes
 const renderCharacters = (data) => {
+  container.innerHTML = "";
   data.results.forEach((character) => {
-    console.log(character);
     container.innerHTML += `<div class="card">
-    <h4>${character.name}</h4>
-        <img src="${character.image}" alt="" />
-        <button class="button" onclick=seeDetails("${character.url}")>Ver detalles</button>
-      </div>`;
+      <h4>${character.name}</h4>
+      <img src="${character.image}" alt="" />
+      <button class="button" onclick=seeDetails("${character.url}")>Ver detalles</button>
+    </div>`;
   });
 };
 
-// Ver detalles de cada uno
+// Ver detalles de cada personaje
 const seeDetails = (characterUrl) => {
   container.innerHTML = "";
   fetch(characterUrl)
     .then((res) => res.json())
     .then((character) => {
-      container.innerHTML = "";
       container.innerHTML = ` <div class="card-detail">
-          <img src="${character.image}" alt="">
+        <img src="${character.image}" alt="">
+        <div class="card-info">
           <h3>${character.name}</h3>
           <p>${character.gender}</p>
           <p>${character.status}</p>
@@ -47,55 +53,72 @@ const seeDetails = (characterUrl) => {
           <p>${character.location.name}</p>
           <p>${character.origin.name}</p>
           <button onclick="BackToHome()">Volver</button>
-        </div>`;
+        </div>
+      </div>`;
     });
 };
 
-// Pruebo agregar esto para volver
+// Función para volver
 const BackToHome = () => {
   window.history.back();
   location.reload();
 };
 
-getCharacters(currentPage);
+// Función para manejar la paginación
+const handlePagination = (pageNumber) => {
+  if (pageNumber <= 1) {
+    currentPage = 1;
+  } else if (pageNumber >= totalPages) {
+    currentPage = totalPages;
+  } else {
+    currentPage = pageNumber;
+  }
+
+  // Actualizar la página de acuerdo al filtro actual
+  getCharacters(currentPage, currentFilter);
+};
 
 // Evento página siguiente
 const nextBtn = document.getElementById("nextButton");
-const previousBtn = document.getElementById("previousButton");
 nextBtn.addEventListener("click", () => {
-  if (currentPage <= 1) {
-    currentPage++;
-  } else if (currentPage > 1 && currentPage < totalPages) {
-    previousBtn.removeAttribute("disabled", false);
-    currentPage++;
-  } else {
-    nextBtn.setAttribute("disabled", true);
-  }
-  getCharacters(currentPage);
+  handlePagination(currentPage + 1);
 });
 
 // Evento página anterior
+const previousBtn = document.getElementById("previousButton");
 previousBtn.addEventListener("click", () => {
-  if (currentPage <= 1) {
-    previousBtn.setAttribute("disabled", true);
-  } else if (currentPage > 1 && currentPage <= totalPages) {
-    currentPage--;
-    nextBtn.removeAttribute("disabled", true);
-  } else {
-    nextBtn.setAttribute("disabled", true);
-    currentPage--;
-  }
-  getCharacters(currentPage);
+  handlePagination(currentPage - 1);
 });
 
-// Evento Primera y última página   --- Hacer disabled
+// Evento Primera página
 const firstButton = document.getElementById("firstBtn");
-const lastButton = document.getElementById("lastBtn");
 firstButton.addEventListener("click", () => {
-  currentPage = 1;
-  getCharacters(currentPage);
+  handlePagination(1);
 });
+
+// Evento Última página
+const lastButton = document.getElementById("lastBtn");
 lastButton.addEventListener("click", () => {
-  currentPage = totalPages;
-  getCharacters(currentPage);
+  handlePagination(totalPages);
 });
+
+// Función para aplicar el filtro por género
+const applyFilter = (filter) => {
+  currentFilter = filter;
+  handlePagination(1); // Volver a la primera página al aplicar un filtro
+};
+
+// Evento para el filtro "Todos"
+allBtn.addEventListener("click", () => applyFilter("all"));
+
+// Evento para el filtro "Mujeres"
+femaleBtn.addEventListener("click", () => applyFilter("Female"));
+
+// Evento para el filtro "Hombres"
+maleBtn.addEventListener("click", () => applyFilter("Male"));
+
+// Evento para el filtro "Desconocidos"
+unknownBtn.addEventListener("click", () => applyFilter("unknown"));
+
+// Inicializar con el filtro "Todos"
+applyFilter("all");
